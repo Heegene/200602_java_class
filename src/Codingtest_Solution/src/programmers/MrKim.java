69f5bccc7328465c9b165a88c6cc5c35cc7a96c0
Heegene <dev.heejin@gmail.com>
2020-06-18 11:58:24 +0900

    200618 Programmers Finding MrKim

--- src/Codingtest_Solution/src/programmers/MrKim.java
+++ src/Codingtest_Solution/src/programmers/MrKim.java
@@ -3,18 +3,19 @@ package Codingtest_Solution.src.programmers;
 import java.util.Arrays;
 
 public class MrKim {
+	static String answer = "";
 	
 	public static void main(String[] args) {
-		Solution sol = new Solution();
-		sol.solution(["Jane","Kim"]);
+		String[] seoul = {"Jane", "Kim"};
+		solution(seoul);
+		System.out.println(answer);
 		
 		
 	}
 
 
 
-	public String solution(String[] seoul) {
-		String answer = "";
+	public static String solution(String[] seoul) {
 		int index = Arrays.asList(seoul).indexOf("Kim");
 		answer = "김서방은" + index + "에 있다";
 		return answer;
