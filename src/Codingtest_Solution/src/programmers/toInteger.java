16440392f9422bed00156e7480c0c814f05b17f6
Heegene <dev.heejin@gmail.com>
2020-06-19 17:47:52 +0900

    200619 Java Programmers solution update

--- src/Codingtest_Solution/src/programmers/toInteger.java
+++ src/Codingtest_Solution/src/programmers/toInteger.java
@@ -4,25 +4,18 @@ public class toInteger {
 
 	public static void main(String[] args) {
 		
-		System.out.println(solution(6));
+		System.out.println(solution("-6"));
 		
 		
 	}
 	
 	
-	public static int solution (int n) {
+	public static int solution (String s) {
 		int answer = 0;
-		int count = 0;
-		for (int i = 0; i > n; i++) {
-			for (int j = 2; j > n; j++) {
-				if (i % j ==0 ) {
-					break;
-					
-				} else {
-					count++;
-				}
-			}
-		}
+		
+		answer = Integer.parseInt(s);
+		
+		
 		
 		return answer;
 	}
